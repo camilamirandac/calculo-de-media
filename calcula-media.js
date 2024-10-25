977c2f38b0c47c4f685d74fe8e0ea00d81aab7e6
Camila Miranda <camilamiranda.dev@gmail.com>
2024-10-24 23:20:53 -0300

    first commit

---
+++ calcula-media.js
@@ -0,0 +1,9 @@
+// posições:
+//             0    1   2   3
+const notas = [10, 6.5, 8, 7.5];
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log('Media das notas:', media);
+
+console.log('Nota da posição 3:', notas[3]);
